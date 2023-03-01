d07c9bf98eaa45904e8d1e7e9835dd7827acb0fc
Luke Vereker <vereker.l@etoncollege.org.uk>
2023-03-01 15:30:37 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Luke Vereker on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
